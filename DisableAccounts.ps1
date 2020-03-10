872da30b02a54d79fc85356685419cd92a5b4767
codeandersen <45607233+codeandersen@users.noreply.github.com>
2020-03-10 21:49:34 +0100

    commit

--- DisableAccounts.ps1
+++ DisableAccounts.ps1
@@ -1,1 +1,40 @@
-test
+    <#
+        .SYNOPSIS
+        Disabling user account from importing a CSV file
+
+        .DESCRIPTION
+        Script is used for automating the decommisiong of user account in Active Directory when employess leaves a company .
+
+        .PARAMETER Name
+        -CSVFile
+            Specifies the location of the Csv file to be imported for disabling accounts.
+            
+            Required?                    true
+               Position?                    0
+               Default value
+               Accept pipeline input?       false
+               Accept wildcard characters?
+
+        .PARAMETER Extension
+        Specifies the extension. "Csv" is the default.
+
+               Required?                    True
+               Position?                    1
+               Default value
+               Accept pipeline input?       false
+               Accept wildcard characters?
+
+        .EXAMPLE
+        C:\PS> DisableAccounts.ps1 -Csvfile "C:\temp\DisableAccounts.csv"
+
+        .COPYRIGHT
+        MIT License, feel free to distribute and use as you like, please leave author information.
+
+       .LINK
+        BLOG: http://www.hcconsult.dk
+        Twitter: @dk_hcandersen
+
+        .DISCLAIMER
+        This script is provided AS-IS, with no warranty - Use at own risk.
+
+    #>
