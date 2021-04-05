0802cf091488f6cca7a3de1e16e20ac4db4e1d12
Hans Christian Andersen <45607233+codeandersen@users.noreply.github.com>
2021-04-05 14:31:54 +0200

    Changes to scripts

--- AddToGroup.ps1
+++ AddToGroup.ps1
@@ -40,22 +40,36 @@
 
     Param(
         [Parameter(Mandatory=$True,
-        Position=0
-        #HelpMessage="Enter path and filename of CSV file"
+        Position=0,
+        HelpMessage="Enter path and filename of CSV file"
         )]
-        [String]$csvfile,
-        [String]$group
+        [String]$csvfile
         )
 
+    Function AddToGroup{
+       
+                $GroupExists = Get-ADGroup -Filter { Name -eq $Group }              
+
+                If($GroupExists)
+                {
+                    Add-ADGroupMember -Identity $Group -Members $Accountname
+	            }
+                Else
+                {
+                    write-output "Group $Group doesn't exist"
+                    Send-MailMessage -From "$MailFrom" -To "$MailTo" -Subject "Add user to group: Error adding user $Accountname to group $Group" -SmtpServer $SmtpServer -UseSsl      
+	            }               
+            
+        }
 
 #Parameters declaration
 $LogFile = "$($env:systemdrive)\logs\AddToGroup_log.txt"
 $SmtpServer = "serv09.tnm.local"
 $MailFrom = "ad@tmg.dk"
 $MailTo = "hca@apento.com"
-$MailSubject = "Add User to AD group report"
-$NewFileNameAfterJob = "\\serv06\scripts\AD\Users\UserOpret.old"
-$OUTopLevelPath = "OU=Afdelinger,OU=Domain Users,DC=tnm,DC=local"
+$MailSubject = "Add users to AD group report"
+#$NewFileNameAfterJob = "\\serv06\scripts\AD\Users\UserOpret.old"
+#$OUTopLevelPath = "OU=Afdelinger,OU=Domain Users,DC=tnm,DC=local"
 
 # Import active directory module for running AD cmdlets
 Import-Module activedirectory
@@ -70,3 +84,39 @@ Import-Module activedirectory
           stop-transcript;
           Exit
       } 
+
+    Try {    
+        #Imports the Csv file
+        $csv = Import-Csv "$csvfile" -Header 'Account','Group' -Delimiter ";" | Select-Object -Skip 1
+        #$csv = Import-csv "\\serv06\scripts\AD\Users\UserOpret.csv" -Header 'Account','Group' -Delimiter ";" | Select-Object -Skip 1
+
+        #Loops through every user and deactivates them
+        ForEach($account in $csv)
+        {
+            #Read user data from each field in each row and assign the data to a variable as below
+            $Accountname 	= $account."Account"
+            $Group 	        = $account."Group"
+                         
+		    AddToGroup
+	                     
+        }
+        
+        #Email is sent with information about users that have been created
+        write-output "Users has been added to group $((get-date).DateTime)"
+        Send-MailMessage -From "$MailFrom" -To "$MailTo" -Subject "$MailSubject" -Body "Add users to group: The following users in the attached file has been added to a group" -Attachments "$csvfile" -SmtpServer $SmtpServer -UseSsl
+        #Remove-Item -Path "$NewFileNameAfterJob" -Confirm:$false -Verbose
+        #endregionRename-Item -Path $csvfile -NewName "$NewFileNameAfterJob"
+}
+#Catch if user creation fails. Logged to file and email sent.
+    catch {
+            write-output "Error: Executed the add users to group script on $((get-date).DateTime) with the error $_"
+            Send-MailMessage -From "$MailFrom" -To "$MailTo" -Subject 'Add users to group: Errror running add users to group script' -Body "Error: Executed the creation user accounts script on $((get-date).DateTime) with the error $_" -SmtpServer $SmtpServer -UseSsl      
+          }           
+
+
+
+
+
+    #Stopping transcript
+    stop-transcript;
+    exit
